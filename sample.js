42c3f91ff1558f8173c29d0d170926eb2dd382c1
itoutamami <itoutamami1114@gmail.com>
2024-06-23 16:34:54 +0900

    TOPページのコーディング

---
+++ sample.js
@@ -0,0 +1,20 @@
+$(function () {
+    const pageTop = $("#page-top");
+    pageTop.hide(); 
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) { 
+        pageTop.fadeIn(); 
+      } else {
+        pageTop.fadeOut(); 
+      }
+    });
+    pageTop.click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0, 
+        },
+        500 
+      );
+      return false;
+    });
+  });
